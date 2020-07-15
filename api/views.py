9388969dcc9a24a87222d4eed6e771d2f959b500
amjadkhan345 <62803260+amjadkhan345@users.noreply.github.com>
2020-07-15 17:53:27 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -81,7 +81,7 @@ def payment(request):
             'show_intermediate_return_page': True,
         }
         url1 = 'https://sandboxapi.rapyd.net/v1/payments'
-        response = requests.post(url, data=body1, headers=headers)
+        response = requests.post(url, body=body1, headers=headers)
 
         #               data=create_payment_body)
         pprint(response)
